14d6c67ac1ce792ebbb61eacdacc58cf987d804c
vicky <vkirtsou@hotmail.com>
2016-11-14 22:31:15 +0200

    better code for muscleCollision script

--- Gamification/Assets/Scripts/MuscleCollision.cs
+++ Gamification/Assets/Scripts/MuscleCollision.cs
@@ -14,68 +14,24 @@ public class MuscleCollision : MonoBehaviour {
 		Muscle6
 	}
 
-	GameObject mus1, mus2, mus3, mus4, mus5, mus6;
+	public GameObject muscle;
 	public FacialMuscleType facialMuscle;
 	private IEnumerator coroutine;
 
 	void Start () {
-		mus1 = GameObject.Find ("Muscle1");
-		mus2 = GameObject.Find ("Muscle2");
-		mus3 = GameObject.Find ("Muscle3");
-		mus4 = GameObject.Find ("Muscle4");
-		mus5 = GameObject.Find ("Muscle5");
-		mus6 = GameObject.Find ("Muscle6");
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		muscle = GameObject.Find (facialMuscle.ToString());
 	}
 
 	void OnTriggerEnter() {
-		
-		switch (facialMuscle) {
-		case FacialMuscleType.Muscle1:
-			coroutine = WaitAndChangeColor (mus1);
-			StartCoroutine (coroutine);
-			Debug.Log ("Muscle 1");
-			break;
-		case FacialMuscleType.Muscle2:
-			coroutine = WaitAndChangeColor (mus2);
-			StartCoroutine (coroutine);
-			Debug.Log ("Muscle 2");
-			break;
-		case FacialMuscleType.Muscle3:
-			coroutine = WaitAndChangeColor (mus3);
-			StartCoroutine (coroutine);
-			Debug.Log ("Muscle 3");
-			break;
-		case FacialMuscleType.Muscle4:
-			coroutine = WaitAndChangeColor (mus4);
-			StartCoroutine (coroutine);
-			Debug.Log ("Muscle 4");
-			break;
-		case FacialMuscleType.Muscle5:
-			coroutine = WaitAndChangeColor (mus5);
-			StartCoroutine (coroutine);
-			Debug.Log ("Muscle 5");
-			break;
-		case FacialMuscleType.Muscle6:
-			coroutine = WaitAndChangeColor (mus6);
-			StartCoroutine (coroutine);
-			Debug.Log ("Muscle 6");
-			break;
-		default:
-			break;
-		}
+		StartCoroutine("WaitAndChangeColor2");
 	}
-
-	private IEnumerator WaitAndChangeColor(GameObject mus) {
+		
+	private IEnumerator WaitAndChangeColor2() {
+		Debug.Log (muscle.ToString ());
 		Color defaultColor;
-		defaultColor = mus.GetComponent<Renderer> ().material.color;	// the current color of the muscle (ie. green)
-		mus.GetComponent<Renderer> ().material.color = Color.red;		// turn muscle red
+		defaultColor = muscle.GetComponent<Renderer> ().material.color;	// the current color of the muscle (ie. green)
+		muscle.GetComponent<Renderer> ().material.color = Color.red;		// turn muscle red
 		yield return new WaitForSeconds (2f);							// wait 2 seconds
-		mus.GetComponent<Renderer> ().material.color = defaultColor;	// turn muscle back to original color
+		muscle.GetComponent<Renderer> ().material.color = defaultColor;	// turn muscle back to original color
 	}
 }
